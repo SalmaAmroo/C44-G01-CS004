43682b72b80d05a1c58cfbeb01006df91dcb80cf
Admin <Admin@SALMAAMRO>
2025-07-12 16:33:03 +0300

    Q1

--- Assigment3CSharp/Program.cs
+++ Assigment3CSharp/Program.cs
@@ -4,6 +4,35 @@
     {
         static void Main(string[] args)
         {
+            #region Q1
+            ////Q1 : 1. Write a program that prints an identity matrix using for loop,
+            ////in other words takes a value n from the user and shows the identity table of size n * n.
+            //Console.WriteLine("Pls Enter the size of Matrix");
+            //int size = int.Parse(Console.ReadLine());
+            //int[,] IdentityMatrix = new int[size, size];
+            //for (int r =0;r<size;r++)
+            //{
+            //    for(int j =0;j<size;j++ )
+            //    {
+            //        if (r == j)
+            //            IdentityMatrix[r, j] = 1;
+            //        else
+            //            IdentityMatrix[r, j] = 0;
+
+            //    }
+            //}
+            //Console.WriteLine($"TheIdentityMatrix {size}*{size}:");
+            //for (int r = 0; r < size; r++)
+            //{
+            //    for (int j = 0; j < size; j++)
+            //    {
+            //        Console.Write(IdentityMatrix[r, j] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
 
         }
     }
